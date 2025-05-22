39dff4c8cc0636a719a911d747fc4e0de4fa7cbc
Tsoomoo012 <btsolmon5555@gmail.com>
2025-05-22 23:49:48 +0800

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Сайн байна уу! Node.js Express сервер рүү тавтай морилно уу!');
+    res.send('Сайн байна уу! Node.js');
 });
 
 app.listen(PORT, () => {
